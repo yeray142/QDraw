02ca563ef117575ca198dc5422c29c8bd51de63e
yeray142 <yeraycorderocarrasco@gmail.com>
2024-05-15 14:40:11 +0200

    New experiments with qfixed

---
+++ notebooks/vector_lstm_quantum_3class_qfixed_seeds.py
@@ -0,0 +1,399 @@
+import logging
+import os
+import random
+import torch
+
+import matplotlib.pyplot as plt
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+
+from qiskit import QuantumCircuit
+from qiskit.circuit import ParameterVector
+from qiskit_machine_learning.connectors import TorchConnector
+from qiskit_machine_learning.neural_networks import SamplerQNN
+from sklearn.model_selection import train_test_split
+from torch.utils.data import TensorDataset, DataLoader
+from tqdm import tqdm
+
+data_folder = '../data/quickdraw'
+
+path = 'quantum_lstm_3class_qfixed'
+if not os.path.exists(path):
+    os.makedirs(path)
+logging.basicConfig(filename=path + '/training_log.txt', level=logging.INFO,
+                    format='%(asctime)s:%(levelname)s:%(message)s')
+
+BATCH_SIZE = 64
+MAX_SEQ_LENGTH = 95
+input_shape = (95, 3)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
+
+
+def interpolate_strokes(strokes, target_length):
+    """Interpolate a sequence of strokes to the target length."""
+    current_length = len(strokes)
+    if current_length == target_length:
+        return strokes
+
+    interpolated_strokes = np.zeros((target_length, 3), dtype=np.float32)
+    interp_indices = np.floor(np.linspace(0, current_length - 1, num=target_length)).astype(int)
+
+    for i, idx in enumerate(interp_indices):
+        interpolated_strokes[i] = strokes[idx]
+
+    for i in range(target_length - 1):
+        if interp_indices[i] != interp_indices[i + 1]:
+            interpolated_strokes[i, 2] = strokes[interp_indices[i], 2]
+        else:
+            interpolated_strokes[i, 2] = 0
+
+    # Ensure the last pen_state remains as the original last one
+    interpolated_strokes[-1, 2] = strokes[-1, 2]
+
+    return interpolated_strokes
+
+
+def resize_and_normalize_drawings(data, size):
+    """Resize and normalize drawing datasets."""
+    resized_data = []
+    for drawing in data['train']:
+        resized_drawing = interpolate_strokes(drawing, size)
+        resized_data.append(resized_drawing)
+    return np.array(resized_data)
+
+
+def prepare_train_test_datasets(camera_drawings, cellphone_drawings, calculator_drawings, test_size=0.2):
+    """
+    Prepare training and testing datasets from the resized drawing data of cameras, cellphones, and calculators.
+
+    Args:
+    camera_drawings (np.array): Resized drawings of cameras.
+    cellphone_drawings (np.array): Resized drawings of cellphones.
+    calculator_drawings (np.array): Resized drawings of calculators.
+    test_size (float): The proportion of the dataset to include in the test split.
+
+    Returns:
+    tuple: Train and test datasets containing features and labels.
+    """
+    camera_labels = np.ones(camera_drawings.shape[0])
+    cellphone_labels = np.ones(cellphone_drawings.shape[0]) * 2
+    calculator_labels = np.zeros(calculator_drawings.shape[0])
+
+    combined_drawings = np.concatenate([camera_drawings, cellphone_drawings, calculator_drawings], axis=0)
+    combined_labels = np.concatenate([camera_labels, cellphone_labels, calculator_labels], axis=0)
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        combined_drawings, combined_labels, test_size=test_size, random_state=42)
+    return X_train, X_test, y_train, y_test
+
+
+def convert_to_tensor_datasets(X_train, X_test, y_train, y_test):
+    """
+    Convert numpy arrays into TensorFlow tensor datasets ready for model training.
+
+    Args:
+    X_train (np.array): Training features.
+    X_test (np.array): Testing features.
+    y_train (np.array): Training labels.
+    y_test (np.array): Testing labels.
+    batch_size (int): Size of batches for the dataset.
+
+    Returns:
+    tuple: TensorFlow datasets for training and testing.
+    """
+    # Convert numpy arrays to TensorFlow tensors
+    train_dataset = TensorDataset(torch.tensor(X_train).to(device), torch.tensor(y_train, dtype=torch.long).to(device))
+    test_dataset = TensorDataset(torch.tensor(X_test).to(device), torch.tensor(y_test, dtype=torch.long).to(device))
+
+    return train_dataset, test_dataset
+
+
+def generate_random_params(num_params, lower_bound=-1, upper_bound=1, seed=None):
+    if seed is not None:
+        random.seed(seed)
+    return [random.uniform(lower_bound, upper_bound) for _ in range(num_params)]
+
+
+def quantum_feature_map(n: int) -> QuantumCircuit:
+    circuit = QuantumCircuit(n)
+    features = ParameterVector("x", n)
+
+    for i in range(n):
+        circuit.rx(features[i], i)
+
+    return circuit
+
+
+def quantum_ansatz(n: int) -> QuantumCircuit:
+    # Creamos un circuito cuántico con n qubits.
+    circuit = QuantumCircuit(n)
+    weights = ParameterVector("w", n * 3)
+
+    # Asumiendo que 'weights' es una lista de valores de peso,
+    # y la longitud es suficiente para aplicar Rz a cada qubit.
+    for i in range(n):
+        circuit.rz(weights[i], i)
+
+    # Aplicar más rotaciones, que en la imagen parecen ser Ry
+    for i in range(n):
+        circuit.ry(weights[n + i], i)
+
+    for i in range(n):
+        circuit.rz(weights[2 * n + i], i)
+
+    # Aplicar entrelazamiento con puertas CNOT
+    for i in range(n - 1):
+        circuit.cx(i, i + 1)
+    circuit.cx(n - 1, 0)
+
+    return circuit
+
+
+# Define and create QNN
+def create_qnn(n, seed):
+    feature_map = quantum_feature_map(n)
+    ansatz = quantum_ansatz(n)
+
+    # Generar valores aleatorios para los parámetros del ansatz con una semilla
+    fixed_params = generate_random_params(3 * n, seed=seed)
+    parameter_values = {param: value for param, value in zip(ansatz.parameters, fixed_params)}
+
+    qc = QuantumCircuit(n)
+    qc.compose(feature_map, inplace=True)
+    qc.compose(ansatz.assign_parameters(parameter_values), inplace=True)
+
+    # REMEMBER TO SET input_gradients=True FOR ENABLING HYBRID GRADIENT BACKPROP
+    qnn = SamplerQNN(
+        circuit=qc,
+        input_params=feature_map.parameters,
+        weight_params=None,
+        input_gradients=True,
+    )
+    return qnn, qc
+
+
+#classes = get_categories_from_data(data_folder)
+classes = ["calculator", "camera", "cell phone"]
+class_id = dict(zip(classes, range(len(classes))))
+
+calculator_data = np.load(f'../data/sketchrnn/sketchrnn_calculator.npz', allow_pickle=True, encoding='latin1')
+camera_data = np.load(f'../data/sketchrnn/sketchrnn_camera.npz', allow_pickle=True, encoding='latin1')
+cellphone_data = np.load(f'../data/sketchrnn/sketchrnn_cell_phone.npz', allow_pickle=True, encoding='latin1')
+
+resized_calculator_drawings = resize_and_normalize_drawings(calculator_data, size=MAX_SEQ_LENGTH)
+resized_camera_drawings = resize_and_normalize_drawings(camera_data, size=MAX_SEQ_LENGTH)
+resized_cellphone_drawings = resize_and_normalize_drawings(cellphone_data, size=MAX_SEQ_LENGTH)
+
+resized_calculator_drawings = resized_calculator_drawings[:5120]
+resized_cellphone_drawings = resized_cellphone_drawings[:5120]
+resized_camera_drawings = resized_camera_drawings[:5120]
+
+X_train, X_test, y_train, y_test = prepare_train_test_datasets(resized_camera_drawings, resized_cellphone_drawings, resized_calculator_drawings, test_size=0.2)
+
+train_dataset, test_dataset = convert_to_tensor_datasets(X_train, X_test, y_train, y_test)
+train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
+
+
+class HybridNN(nn.Module):
+    def __init__(self, input_shape, num_categories, n_qubits=5, fname=None, seed=0):
+        super(HybridNN, self).__init__()
+        self.num_categories = num_categories
+
+        # Convolutional layers
+        self.conv1 = nn.Conv1d(in_channels=input_shape[1], out_channels=16, kernel_size=4)
+        self.bn1 = nn.BatchNorm1d(num_features=16)
+        self.conv2 = nn.Conv1d(in_channels=16, out_channels=32, kernel_size=5)
+        self.bn2 = nn.BatchNorm1d(num_features=32)
+        self.conv3 = nn.Conv1d(in_channels=32, out_channels=64, kernel_size=3)
+        self.bn3 = nn.BatchNorm1d(num_features=64)
+        self.pool1 = nn.MaxPool1d(kernel_size=2)
+        self.conv4 = nn.Conv1d(in_channels=64, out_channels=128, kernel_size=3)
+        self.bn4 = nn.BatchNorm1d(num_features=128)
+        self.pool2 = nn.MaxPool1d(kernel_size=2)
+
+        # LSTM layers
+        self.lstm1 = nn.LSTM(input_size=128, hidden_size=128, batch_first=True)
+        self.lstm2 = nn.LSTM(input_size=128, hidden_size=128, batch_first=True)
+        self.pool3 = nn.MaxPool1d(kernel_size=2)
+
+        # Dense layers
+        self.fc1 = nn.Linear(64, 64)
+        self.fc2 = nn.Linear(64, 64)
+
+        # Quantum layer
+        self.fc3 = nn.Linear(64, n_qubits)
+        self.qnn = TorchConnector(
+            create_qnn(n_qubits, seed)[0]
+        )
+
+        # Dense layer for classification
+        self.fc4 = nn.Linear(2 ** n_qubits, num_categories)
+
+        # Load pre-trained weights
+        if fname:
+            self.load_state_dict(torch.load(fname))
+
+    def forward(self, x):
+        x = x.permute(0, 2, 1)
+
+        # Convolutional layers
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = self.pool1(x)
+        x = F.relu(self.bn4(self.conv4(x)))
+        x = self.pool2(x)
+
+        # Reshape for LSTM
+        x = x.permute(0, 2, 1)
+        x, _ = self.lstm1(x)
+        x, _ = self.lstm2(x)
+        x = x[:, -1, :]
+
+        # Pool layer
+        x = self.pool3(x)
+
+        # Flatten the output for dense layer
+        x = torch.flatten(x, 1)
+
+        # Dense layers
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+
+        # Quantum layer
+        x = F.relu(self.fc3(x))
+        x = self.qnn(x)
+
+        # Dense layer for classification
+        x = F.relu(self.fc4(x))
+        return x
+
+    def predict(self, x):
+        self.eval()
+        with torch.no_grad():
+            x = torch.tensor(x, dtype=torch.float32)
+            prediction = self.forward(x)
+        return prediction.numpy()
+
+
+criterion = nn.CrossEntropyLoss()
+
+
+# Function to evaluate the model on validation data
+def evaluate(model, test_loader):
+    model.eval()
+    total_correct = 0
+    total_loss = 0
+    total_samples = 0
+    with torch.no_grad():
+        for inputs, targets in test_loader:
+            outputs = model(inputs)
+            _, predicted = torch.max(outputs.data, 1)
+            total_samples += targets.size(0)
+
+            total_loss += criterion(outputs, targets)
+            total_correct += (predicted == targets).sum().item()
+    model.train()
+    return total_correct / total_samples, total_loss / len(test_loader)
+
+
+def plot_graphs(loss_list, val_loss_list, accuracy_list, val_accuracy_list, iteration, output_dir='plots'):
+    try:
+        # Ensure the output directory exists
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+        plt.figure(figsize=(10, 5))
+
+        # Plotting loss
+        plt.subplot(1, 2, 1)
+        plt.plot(loss_list, label='Training Loss')
+        plt.plot(val_loss_list, label='Validation Loss')
+        plt.title('Training and Validation Loss')
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
+        plt.legend()
+
+        # Plotting accuracy
+        plt.subplot(1, 2, 2)
+        plt.plot(accuracy_list, label='Training Accuracy')
+        plt.plot(val_accuracy_list, label='Validation Accuracy')
+        plt.title('Training and Validation Accuracy')
+        plt.xlabel('Epoch')
+        plt.ylabel('Accuracy')
+        plt.legend()
+
+        plt.tight_layout()  # Adjust layout to prevent overlap
+        plt.savefig(os.path.join(output_dir, f'iteration_{iteration + 1}.png'))
+    except Exception as e:
+        print(f"Error while plotting graphs: {e}")
+    finally:
+        plt.close('all')  # Ensure all figures are closed
+
+
+for iteration in range(100):
+    seed = iteration
+
+    # Crear el modelo con nuevos parámetros cuánticos aleatorios
+    model = HybridNN(input_shape, len(classes), seed=seed)
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    # Training loop
+    best_accuracy = 0.0
+    accuracy_list = []
+    loss_list = []
+    val_accuracy_list = []
+    val_loss_list = []
+
+    for epoch in range(50):
+        model.train()
+
+        total_loss = []
+        total_correct = 0
+        total_samples = 0
+
+        for inputs, labels in tqdm(train_loader):
+            # Forward pass
+            optimizer.zero_grad(set_to_none=True)
+            outputs = model(inputs)
+
+            # Calculate loss
+            loss = criterion(outputs, labels)
+            loss.backward()
+
+            _, predicted = torch.max(outputs.data, 1)
+            total_correct += (predicted == labels).sum().item()
+
+            # Backward pass
+            optimizer.step()
+            total_loss.append(loss.item())
+            total_samples += 1
+
+        # Evaluate the model on the validation data after each epoch
+        val_accuracy, val_loss = evaluate(model, test_loader)
+        val_accuracy_list.append(val_accuracy)
+        val_loss_list.append(val_loss)
+
+        accuracy_list.append(total_correct / len(train_loader.dataset))
+        loss_list.append(sum(total_loss) / len(train_loader))
+
+        logging.info(
+            f'Iteration {iteration + 1}, Epoch {epoch + 1}, Accuracy: {total_correct / len(train_loader.dataset)}, Loss: {sum(total_loss) / len(train_loader)}, Validation Accuracy: {val_accuracy}, Validation Loss: {val_loss}')
+
+        # Checkpointing
+        if val_accuracy > best_accuracy:
+            best_accuracy = val_accuracy
+            torch.save(model.state_dict(), f'quantum_lstm_3class_qfixed/best_model_iteration_{iteration + 1}.pth')
+
+        # Guardar los parámetros entrenables en un archivo .npz
+        params_dict = {name: param.data.cpu().numpy() for name, param in model.named_parameters() if
+                       param.requires_grad}
+        np.savez(f'quantum_lstm_3class_qfixed/parameters_iteration_{iteration + 1}_epoch_{epoch + 1}.npz',
+                 **params_dict)
+
+    plot_graphs(loss_list, val_loss_list, accuracy_list, val_accuracy_list, iteration)
